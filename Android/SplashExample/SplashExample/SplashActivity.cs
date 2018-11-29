7dd8d307206845609873892852398b91a81c6fa6
Kaspar Rosin <Kaspar.Rosin@tptlive.ee>
2018-11-29 09:15:14 +0200

    splash screen

---
+++ Android/SplashExample/SplashExample/SplashActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace SplashExample
+{
+    [Activity(Label = "SplashActivity", Theme = "@style/SplashTheme",MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X: " + typeof(SplashActivity).Name;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SupportActionBar.Hide();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+        async void SimulateStartup()
+        {
+            await Task.Delay(8000);
+            StartActivity(new Intent(Application.Context, typeof(SplashActivity)));
+        }
+    }
+}
